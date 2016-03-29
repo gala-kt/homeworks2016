ffca3cb4c5f8053b4f82524b9ace735027bab20a
admin <admin@gala-pc>
2016-03-30 02:42:23 +0300

    Change Task02: reverse.

--- homeworks/src/homework160329/Task02.java
+++ homeworks/src/homework160329/Task02.java
@@ -5,6 +5,8 @@ public class Task02 {
 	public static void main(String[] args) {
 		
 		System.out.println("1 2 3 4 5 6 7 8 9 10");
+		System.out.println("Reverse!");
+		System.out.println("10 9 8 7 6 5 4 3 2 1");
 
 	}
 
